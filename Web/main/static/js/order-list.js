62daaadd12831aeae9d38ddf298f30e9ae26c181
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2023-08-08 20:03:45 +0900

    프론트/백

---
+++ Web/main/static/js/order-list.js
@@ -0,0 +1,107 @@
+const order_list = document.querySelector('.ordered-list');
+const order_result = document.getElementById('result-price');
+
+let result_price = 0;   // 총 주문 금액
+let item_list = [];     // 주문 리스트
+
+
+order_result.innerText = result_price.toLocaleString('ko-KR') + '원';
+
+/**
+ * object 정보에 해당하는 주문 item을 html 문서에 추가
+ * 
+ * add_item 메소드에서 사용
+ * 
+ * @param {*} object 
+ */
+function add_item_html(object) {
+    const menu_name = object.name;
+    const menu_img = object.img;
+    const menu_price = object.price;
+    const menu_etc = object.etc;
+
+    result_price += menu_price;
+
+    const ordered_item = document.importNode(document.querySelector('#temp').content, true);
+
+    ordered_item.querySelector('.ordered-item').setAttribute('id', menu_name);
+    ordered_item.querySelector('.menu-name').textContent = menu_name;
+    ordered_item.getElementById('menu-img').setAttribute('src', menu_img);
+    ordered_item.querySelector('#menu-price').textContent = menu_price.toLocaleString('ko-KR') + ' 원';
+    ordered_item.getElementById('menu-etc').innerText = menu_etc;
+
+    order_list.append(ordered_item);
+    order_result.innerText = result_price.toLocaleString('ko-KR') + '원';
+};
+
+
+/**
+ * object 정보에 해당하는 주문 item을 html 문서에서 삭제
+ * 
+ * delete_item 메소드에서 사용
+ * 
+ * @param {*} object 
+ */
+function delete_item_html(object) {
+    const menu_name = object.name;
+    const menu_price = object.price;
+
+    const item = document.getElementById(menu_name);
+    item.remove();
+    item_list
+    result_price -= menu_price;
+    order_result.innerText = result_price.toLocaleString('ko-KR') + '원';
+};
+
+/**
+ * object에 해당하는 주문 item을 추가
+ * 
+ * @param {*} object -
+ * - object.name(String): 메뉴의 이름
+ * - object.img(String): 메뉴 이미지의 저장 위치
+ * - object.price(Number): 메뉴 가격
+ * - object.etc(String): 메뉴 세부 정보 (ex. 단품/세트, 치즈 추가, 토마토 제외 등...)
+ */
+function add_item(object) {
+    add_item_html(object);
+    item_list.push(object);
+};
+
+/**
+ * object에 해당하는 주문 item을 삭제
+ * 
+ * @param {*} object -
+ * - object.name(String): 메뉴의 이름
+ * - object.img(String): 메뉴 이미지의 저장 위치
+ * - object.price(Number): 메뉴 가격
+ * - object.etc(String): 메뉴 세부 정보 (ex. 단품/세트, 치즈 추가, 토마토 제외 등...)
+ */
+function delete_item(object) {
+    let index_of_item = item_list.indexOf(object);
+
+    delete_item_html(object);
+    item_list.splice(index_of_item, 1);
+};
+
+//----- add_item 테스트 -----//
+
+
+for (let i = 0; i < 10; i++) {
+    let menu_name = '메뉴' + String(i + 1);
+    let menu_img = "static/img/pubao_1.webp";
+    let menu_price = Math.floor(Math.random() * 50000);
+    let menu_etc = '테스트 테스트 테스트';
+
+
+
+    let object = { name: menu_name, img: menu_img, price: menu_price, etc: menu_etc };
+
+    add_item(object);
+};
+
+for (let i = 0; i < 4; i++) {
+    let random_object = item_list[Math.floor(Math.random() * item_list.length)];
+
+    delete_item(random_object);
+};
+
---
+++ Web/main/static/js/recoder.js
@@ -0,0 +1,58 @@
+const record = document.getElementById('record_button');
+const audio = document.getElementById('audioPlayer');
+let isRecording = false;
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({audio: true})
+    .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+
+        record.onclick = () => {
+            if (!isRecording){
+                let chunks = [];
+
+                mediaRecorder.ondataavailable = async (event) => {
+                    chunks.push(event.data);
+                };
+
+                mediaRecorder.onstop = (event) => {
+                    // chunks.forEach(async (data) => {
+                    //     // console.log(data);
+                    //     let temp = await data.text();
+                    //     console.log(temp);
+                    // });
+
+                    const recordData = new Blob(chunks, {"type": "audio/mpeg codecs=opus"});
+
+                    chunks.splice(0);
+
+                    const recordURL = window.URL.createObjectURL(recordData);
+
+                    audio.src = recordURL;
+                    audio.play();
+                };
+
+                mediaRecorder.start(1000);
+                isRecording = true;
+
+                record.style.background = "red";
+                record.style.color = "black";
+
+                setTimeout(() => {
+                    if (mediaRecorder.state = 'recording') {
+                        mediaRecorder.stop();
+
+                        record.style.background = "";
+                        record.style.color = "";
+                    };
+
+                    isRecording = false;
+                }, 8000)
+            };
+        };
+    }).catch((err) => {
+        alert("음성 녹음 준비 단계에서 오류가 발생했습니다!\n마이크 사용 허용이 되어있지 않다면 마이크 사용을 허용해주세요.");
+    });
+} else {
+    alert("현재 브라우저가 음성 녹음을 지원하지 않습니다!\n(지원 브라우저: Chrome)");
+};
