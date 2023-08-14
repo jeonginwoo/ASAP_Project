263f976a9afbeefa8e32a4eae5e05557e724e98d
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2023-08-14 16:37:50 +0900

    테이블 생성하기 연습

--- Web/config/urls.py
+++ Web/config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from mains.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,5 @@ urlpatterns = [
     path('main/', include('mains.urls')),
     path('', index, name='index'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Web/mains/models.py
+++ Web/mains/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Menu(models.Model):
-    image = models.ImageField(verbose_name='이미지', null=True, blank=True)
+    image = models.ImageField(upload_to='static/img/BURGERKING_MENU/Burger/', verbose_name='이미지', null=True, blank=True)
     name = models.CharField(max_length=25, verbose_name='메뉴명')
     price = models.IntegerField(verbose_name='가격', default=10000)
     calorie = models.FloatField(verbose_name='칼로리', default=100.5)
