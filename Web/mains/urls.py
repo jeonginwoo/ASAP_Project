133b8b84e1099c01afeafa00cd912b0353ed9762
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2023-08-15 18:18:35 +0900

    정리

--- Web/mains/urls.py
+++ Web/mains/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'mains'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('Menu/<str:menu_key>/', views.MenuDetailView.as_view(), name='menu-detail'),
     path('purchase/', views.purchase, name='purchase'),
     path('speechrecognize/', views.speechRecognition, name="speechRecognition"),
