92517462cf478aae96995d00cf6f2f586764a646
cha-imag <88891682+cha-imag@users.noreply.github.com>
2023-08-16 20:17:06 +0900

    Ko_bert 클래스화

--- Model/Ko_Bert/main.py
+++ Model/Ko_Bert/main.py
@@ -5,16 +5,15 @@ from CustomPredictor import CustomPredictor
 
 CHECKPOINT_NAME = 'chaimag/Bert_3'
 
-device = torch.device('cpu')
-
-config = BertConfig.from_pretrained(CHECKPOINT_NAME)
-model_bert = BertModel.from_pretrained(CHECKPOINT_NAME).to(device)
-tokenizer = BertTokenizer.from_pretrained(CHECKPOINT_NAME)
-
-bert = CustomBertModel(CHECKPOINT_NAME).to(device)
-bert.load_state_dict(torch.load('C:/Users/tkdal/Desktop/바트/bert-kor-base.pth',map_location=torch.device('cpu')))
-
-labels = {
+class Ko_Bert():
+    def __init__(self):
+        self.device = torch.device('cpu')
+        self.config = BertConfig.from_pretrained(CHECKPOINT_NAME)
+        self.model_bert = BertModel.from_pretrained(CHECKPOINT_NAME).to(self.device)
+        self.tokenizer = BertTokenizer.from_pretrained(CHECKPOINT_NAME)
+        self.bert = CustomBertModel(CHECKPOINT_NAME).to(self.device)
+        self.bert.load_state_dict(torch.load('C:/Users/tkdal/Desktop/바트/bert-kor-base.pth',map_location=torch.device('cpu')))
+        self.labels = labels = {
     0: '추천 입니다',
     1: '남성 입니다',
     2 : '여성 입니다',
@@ -28,12 +27,18 @@ labels = {
     10 : '겨울입니다',
     11 : '기타'
 }
+        self.pre = CustomPredictor(self.bert, self.tokenizer, self.labels,self.device)
+        
 
-# CustomPredictor 인스턴스를 생성합니다.
-predictor = CustomPredictor(bert, tokenizer, labels,device)
 
-# 사용자 입력에 대하여 예측 후 출력을 낼 수 있는 간단한 함수를 생성합니다.
-def predict_sentence(predictor):
-    input_sentence = input('문장을 입력해 주세요: ')
-    predictor.predict(input_sentence)
-predict_sentence(predictor)
+    # 사용자 입력에 대하여 예측 후 출력을 낼 수 있는 간단한 함수를 생성합니다.
+    def predict_sentence(self,pre):
+        input_sentence = input('문장을 입력해 주세요: ')
+        pre.predict(input_sentence)
+    
+    def Start_bert(self):
+        self.predict_sentence(self.pre)
+
+
+K = Ko_Bert()
+K.Start_bert()
